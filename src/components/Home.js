ed18341c13d9bd94a2d411181c9d1d9f0f5ee067
tushargaur3 <120725354+tushargaur3@users.noreply.github.com>
2023-01-29 23:25:29 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-//import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 const Home = () => {
     const navigate = useNavigate()
-    
     return (
         <div id='home-page'>
 
